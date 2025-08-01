c64b5a205b8a1607f7005a94d39d1705e52fe31b
Ty/cam_0 <136151272+camera-Z3ro@users.noreply.github.com>
2025-08-01 12:27:23 -0400

    Window Creation

---
+++ window.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace UniSim
+{
+    public class Window
+    {
+        public Window()
+        {
+            // On macOS, you can use Avalonia or Eto.Forms for cross-platform UI.
+            // Here is a minimal example using Eto.Forms:
+
+            // Make sure to add the Eto.Forms NuGet package:
+            // dotnet add package Eto.Forms
+
+            var app = new Eto.Forms.Application(Eto.Platforms.Mac64);
+            var form = new Eto.Forms.Form
+            {
+                Title = "UniSim Window",
+                ClientSize = new Eto.Drawing.Size(400, 300)
+            };
+            app.Run(form);
+        }
+    }
+}
